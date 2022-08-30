f357c3edb9530b6cd8817809cdd60365e50acf55
brennoaq <brennoaraujoqueiroz@gmail.com>
2022-08-30 11:56:47 -0300

    add wallet on display

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,19 +18,17 @@ const Home: NextPage = () => {
   async function connectWithLedger(){
     try {
       // Keep if you chose the USB protocol
-      // const transport = await TransportWebUSB.create();
-      const transportWebUSB = await TransportWebUSB.create();
+      const transport = await TransportWebUSB.create();
   
       // Keep if you chose the HID protocol
-      // const transport = await TransportWebHID.create();
-      const transportWebHID = await TransportWebHID.create();
+      const transport = await TransportWebHID.create();
   
       //listen to the events which are sent by the Ledger packages in order to debug the app
       listen(log => console.log(log))
   
       //When the Ledger device connected it is trying to display the bitcoin address
 
-      const appBtc = new AppBtc(transportWebHID);
+      const appBtc = new AppBtc(transport);
       
       const { bitcoinAddress } = await appBtc.getWalletPublicKey(
         "44'/0'/0'/0/0",
@@ -61,11 +59,11 @@ const Home: NextPage = () => {
         </h1>
 
         <div className={styles.description}>
-        {account?'Your first Bitcoin address:' : 'Connect your Nano and open the Bitcoin app. Click on button to start...'} 
+        {account?`Your first Bitcoin address: ${account}` : 'Connect your Nano and open the Bitcoin app. Click on button to start...'} 
         </div>
 
         <Flex
-          h="100vh"
+          h="100%"
           justifyContent="center"
           alignItems="center">
           <Button
